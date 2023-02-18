781fdcbd1b2543172a54739d471d162825a650c3
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-02-18 07:45:37 +0000

    thredday

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :index]
   resources :books, only: [:new, :index, :show, :create]
   devise_for :users
-  get 'homes/top'
+  get 'homes/about'
   root to: "homes#top"
 end
